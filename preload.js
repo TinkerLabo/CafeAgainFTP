574a282f5f51c5c5848826cdfa7d9b50a360405a
Tinkerlabo <simujyaky@gmail.com>
2021-09-12 17:29:18 +0900

    add sources

---
+++ preload.js
@@ -0,0 +1,25 @@
+const { remote } = require('electron');
+const { dialog, BrowserWindow } = remote;
+const Encoding = require('encoding-japanese');
+
+window.remote = remote;
+window.BrowserWindow = BrowserWindow;
+window.dialog = dialog;
+window.Encoding = Encoding;
+
+//require('./renderer.js')
+const sharp = require('sharp');
+window.sharp = sharp;
+
+const fs = require('fs');
+window.fs = fs;
+
+const path = require('path');
+window.path = path;
+
+const Diff = require('diff');
+window.Diff = Diff;
+
+// window.jQuery = window.$ = require('./render/js/jquery-3.6.0.min.js');
+
+//window.jQuery = window.$ = require('./lib/jquery');
